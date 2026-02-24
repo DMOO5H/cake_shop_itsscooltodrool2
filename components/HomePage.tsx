c1fbba62a10cc87890efa95e86ef6832fcc8bb7e
DMOO5H <maulanahilmydarrryll@gmail.com>
2026-02-24 11:50:22 +0700

    update

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -4,13 +4,11 @@ import Hero from "./Hero";
 import AboutUs from "./AboutUs";
 import FeatureSection from "./FeatureSection";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
 
 
 export default function HomePage() {
   return (
     <div>
-      <Navbar />
       <Hero />
       <AboutUs />
       <FeatureSection />
