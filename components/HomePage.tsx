bfaf365ed873ba5db328779457bc9222b970a20e
DMOO5H <maulanahilmydarrryll@gmail.com>
2026-02-24 11:52:04 +0700

    fixing error build

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -4,11 +4,13 @@ import Hero from "./Hero";
 import AboutUs from "./AboutUs";
 import FeatureSection from "./FeatureSection";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 
 export default function HomePage() {
   return (
     <div>
+      <Navbar />
       <Hero />
       <AboutUs />
       <FeatureSection />
